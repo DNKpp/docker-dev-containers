f38e3c0d62e79ca8da4bb5dc0357cfb6670b31c9
DNKpp <dnkpp2011@gmail.com>
2025-06-15 15:00:42 +0200

    add arm based base images

--- tools/install-cmake.sh
+++ tools/install-cmake.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -eou pipefail
 
-VERSION="3.30.6"
+VERSION="3.31.8"
 
+ARCH="$(uname -m)"
 MIRROR_URL="https://github.com/Kitware/CMake/releases/download/v${VERSION}/"
-DOWNLOAD_SH="cmake-${VERSION}-linux-x86_64.sh"
+DOWNLOAD_SH="cmake-${VERSION}-linux-${ARCH}.sh"
 DOWNLOAD_FILE="cmake.sh"
 
 # Download
